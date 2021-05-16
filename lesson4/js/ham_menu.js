a10afdb6d7d0d4bca19a4bab1e22e5ec63b995a4
rhlmeena95 <66035918+rhlmeena95@users.noreply.github.com>
2021-05-16 17:59:10 +0530

    Delete lesson4 directory

--- lesson4/js/ham_menu.js
+++
@@ -1,4 +0,0 @@
-function toggleMenu() {
-    document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
-}
-window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
